f39c0c1c4c5745f80785202ea3e966d52d555957
TuomasJalonen <jalonen3@wks-92749-mac.ad.tuni.fi>
2020-05-22 19:28:24 +0300

    GCloud settings

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   // API_ENDPOINT: 'http://localhost:5000/api',
   // API_ENDPOINT: 'https://[YOUR-ROUTE].mybluemix.net/api',
-    API_ENDPOINT: 'https://tj-classifier.herokuapp.com/',
+    API_ENDPOINT: 'https://tj-classifier.ew.r.appspot.com',
 };
