b17c1e3b36c0c0587e4fcfc8eb395c45779da105
TuomasJalonen <jalonen3@wks-92749-mac.ad.tuni.fi>
2020-05-22 13:05:44 +0300

    Add to Heroku

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:5000/api',
-  // API_ENDPOINT: 'https://[YOUR-ROUTE].mybluemix.net/api'
+  // API_ENDPOINT: 'http://localhost:5000/api',
+  // API_ENDPOINT: 'https://[YOUR-ROUTE].mybluemix.net/api',
+    API_ENDPOINT: 'https://tj-classifier.herokuapp.com/',
 };
