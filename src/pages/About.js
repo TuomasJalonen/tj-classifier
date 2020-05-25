9864f234ff1e211928a9a29f1987bc224fd5d37f
TuomasJalonen <jalonen3@wks-92749-mac.ad.tuni.fi>
2020-05-25 18:24:08 +0300

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,10 +12,12 @@ export default class Home extends Component {
         <h1>About</h1>
         <p>
           This is a TensorFlow.js web application where users can classify images selected locally
-          or taken with their device's camera. The app uses TensorFlow.js and a pre-trained model
-          converted to the TensorFlow.js format to provide the inference capabilities.
-          This model is saved locally in the browser using IndexedDB. A service worker is also used
-          to provide offline capabilities.
+          or taken with their device's camera. The app uses a neural network, which can be changed according to your needs to solve your problem. If you're interested in making this to benefit your business, please contact me on
+            
+            <script type="text/javascript">
+            document.write('https://www.linkedin.com/in/tuomasjalonen/'.link('https://www.linkedin.com/in/tuomasjalonen/'));
+            </script>
+            .
         </p>
       </div>
     );
